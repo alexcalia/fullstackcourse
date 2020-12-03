71be579dda0c4210a25ece0519b170f6412c1927
Alex Calia <alex.m.calia@outlook.com>
2020-12-03 17:39:27 -0500

    further alonmg he nodejs lesson

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let notes = [
   {
     id: 1,
@@ -22,6 +24,22 @@ let notes = [
   }
 ]
 
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+
+  return maxId + 1
+}
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+}
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 })
@@ -39,7 +57,26 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end();
   }
-  
+});
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+  if (!body.content) {
+    return response.status(404).json({
+      error: 'content missing'
+    });
+  };
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note);
+
+  response.json(note);
 })
 
 app.delete('/api/notes/:id', (request, response) => {
