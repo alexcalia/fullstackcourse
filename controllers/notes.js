90ca23f18e42de3d25b465af1f2e4e283e1328c5
Alex Calia <alex.m.calia@outlook.com>
2020-12-13 19:21:50 -0500

    installedl ibrary to handle try/catch errors, simplified code

--- controllers/notes.js
+++ controllers/notes.js
@@ -7,34 +7,31 @@ notesRouter.get('/', (request, response) => {
   })
 })
 
-notesRouter.get('/', async (request, response) => { 
-  const notes = await Note.find({})
-  response.json(notes)
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date()
+    date: new Date(),
   })
 
-  try { 
-    const savedNote = await note.save()
-    response.json(savedNote)
-  } catch(exception) {
-    next(exception)
-  }
+  const savedNote = await note.save()
+  response.json(savedNote)
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 notesRouter.put('/:id', (request, response, next) => {
