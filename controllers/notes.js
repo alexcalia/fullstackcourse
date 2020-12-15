d66c61ade1609f15d5074a54e98cccf389bf5093
Alex Calia <alex.m.calia@outlook.com>
2020-12-15 12:12:50 -0500

    added error handling for invalid or missing tokens

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,16 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+
+  return null
+}
 
 notesRouter.get('/', (request, response) => {
   Note.find({}).then(notes => {
@@ -17,8 +27,13 @@ notesRouter.get('/', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({error: 'token missing or invalid'})
+  }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
